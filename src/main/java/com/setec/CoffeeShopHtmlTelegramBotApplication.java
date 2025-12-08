0df00945a6fe2a686556928b1c256b23635e2dbc
Thanyang <Theanyang@example.com>
2025-12-08 16:41:51 +0700

    Initial commit

--- src/main/java/com/setec/CoffeeShopHtmlTelegramBotApplication.java
+++ src/main/java/com/setec/CoffeeShopHtmlTelegramBotApplication.java
@@ -1,13 +1,19 @@
 package com.setec;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoffeeShopHtmlTelegramBotApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CoffeeShopHtmlTelegramBotApplication.class, args);
 	}
+	
+    void contextLoads() {
+        // Test will pass if Spring Boot context loads
+    }
+
 
 }
